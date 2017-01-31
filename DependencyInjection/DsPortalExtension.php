003aa7cf26d594fc719c7fa2e3b020910cc7a92d
mario-digitalstate <mario@digitalstate.ca>
2017-01-31 15:19:25 -0500

    Add copyright widget.

--- DependencyInjection/DsPortalExtension.php
+++ DependencyInjection/DsPortalExtension.php
@@ -23,6 +23,7 @@ class DsPortalExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
         $loader->load('listeners.yml');
+        $loader->load('widgets.yml');
 
         $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip([ 'settings' ])));
     }
